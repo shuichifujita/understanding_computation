222381062d99e003dc00b26b84f18a513359afe4
Yohei Okubo <okubo.yohei@gmail.com>
2017-03-12 23:42:33 +0900

    inspect時の表示実装

--- small_step_semantics/src/expressions.rb
+++ small_step_semantics/src/expressions.rb
@@ -1,8 +1,29 @@
+module Inspectable
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
 class Number < Struct.new(:value)
+  include Inspectable
+
+  def to_s
+    value.to_s
+  end
 end
 
 class Add < Struct.new(:left, :right)
+  include Inspectable
+
+  def to_s
+    "#{left} + #{right}"
+  end
 end
 
 class Multiply < Struct.new(:left, :right)
+  include Inspectable
+
+  def to_s
+    "#{left} * #{right}"
+  end
 end
