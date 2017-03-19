eae55c50c9b393ccec058e1375f70bdb1e222bd7
Shuichi Fujita <chaashuuu@gmail.com>
2017-03-20 01:58:53 +0900

    シーケンス文の実装

--- operational_semantics/NOTE.md
+++ operational_semantics/NOTE.md
@@ -102,3 +102,15 @@ Machine.new(
 ).run
 ```
 
+## p.37 シーケンス文
+
+```ruby
+Machine.new(
+  Sequence.new(
+    Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
+    Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
+  ),
+  {}
+).run
+```
+
--- operational_semantics/src/expressions.rb
+++ operational_semantics/src/expressions.rb
@@ -213,3 +213,26 @@ class Machine < Struct.new(:statement, :environment)
   end
 end
 
+
+class Sequence < Struct.new(:first, :second)
+  include Inspectable
+
+  def to_s
+    "#{first}; #{second}"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    case first
+    when DoNothing.new
+      [second, environment]
+    else
+      reduced_first, reduced_environment = first.reduce(environment)
+      [Sequence.new(reduced_first, second), reduced_environment]
+    end
+  end
+end
+
