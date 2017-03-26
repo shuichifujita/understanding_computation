7d3fe453e3b48fdc8debee695ff7be707d66f049
march-am <myredblood@gmail.com>
2017-03-26 16:42:07 +0900

    Big-step Semantics

---
+++ operational_semantics/src/big-step_semantics.rb
@@ -0,0 +1,164 @@
+#Big-Step Semantics
+
+module Inspectable
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+
+class Number < Struct.new(:value)
+  include Inspectable
+
+  def to_s
+    value.to_s
+  end
+
+  def evaluate(environment)
+    self
+  end
+end
+
+
+class Boolean < Struct.new(:value)
+  include Inspectable
+
+  def to_s
+    value.to_s
+  end
+
+  def evaluate(environment)
+    self
+  end
+end
+
+
+class Variable < Struct.new(:name)
+  include Inspectable
+
+  def to_s
+    name.to_s
+  end
+
+  def evaluate(environment)
+    environment[name]
+  end
+end
+
+
+class Add < Struct.new(:left, :right)
+  include Inspectable
+
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+  end
+end
+
+
+class Multiply < Struct.new(:left, :right)
+  include Inspectable
+
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value * right.evaluate(environment).value)
+  end
+end
+
+
+class LessThan < Struct.new(:left, :right)
+  include Inspectable
+
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def evaluate(environment)
+    Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
+  end
+end
+
+
+class DoNothing
+  include Inspectable
+
+  def to_s
+    "do-nothing"
+  end
+
+  def ==(other_statement)
+    other_statement.instance_of?(DoNothing)
+  end
+
+  def evaluate(environment)
+    environment
+  end
+end
+
+
+class Assign < Struct.new(:name, :expression)
+  include Inspectable
+
+  def to_s
+    "#{name} = #{expression}"
+  end
+
+  def evaluate(environment)
+    environment.merge({ name => expression.evaluate(environment) })
+  end
+end
+
+
+class If < Struct.new(:condition, :consequence, :alternative)
+  include Inspectable
+
+  def to_s
+    "if (#{condition}) { #{consequence} } else { #{alternative} }"
+  end
+
+  def evaluate(environment)
+    case condition.evaluate(environment)
+    when Boolean.new(true)
+      consequence.evaluate(environment)
+    when Boolean.new(false)
+      alternative.evaluate(environment)
+    end
+  end
+end
+
+
+class Sequence < Struct.new(:first, :second)
+  include Inspectable
+
+  def to_s
+    "#{first}; #{second}"
+  end
+
+  def evaluate(environment)
+    second.evaluate(first.evaluate(environment))
+  end
+end
+
+
+class While < Struct.new(:condition, :body)
+  include Inspectable
+
+  def to_s
+    "while (#{condition}) { #{body} }"
+  end
+
+  def evaluate(environment)
+    case condition.evaluate(environment)
+    when Boolean.new(true)
+      evaluate(body.evaluate(environment))
+    when Boolean.new(false)
+      environment
+    end
+  end
+end
