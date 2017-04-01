38ea1d68066d542c5201b56e1f0a04c84f1d59b4
Yohei Okubo <okubo.yohei@gmail.com>
2017-04-01 15:01:39 +0900

    Add, Multipl, LessThan

--- operational_semantics/big_step/src/definitions.rb
+++ operational_semantics/big_step/src/definitions.rb
@@ -19,3 +19,26 @@ class Variable # args = {:name}
     environment[name]
   end
 end
+
+class Add
+  def evaluate(environment)
+    ###########################################################################
+    # Requiring recursive evaluation of their left and right subexpressions
+    # before combining both values with the appropriate Ruby operator.
+    # -->> Means, wait here until end of left expression's evaluation.
+    ###########################################################################
+    Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+  end
+end
+
+class Multiply # args = {:left, :right}
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value * right.evaluate(environment).value)
+  end
+end
+
+class LessThan
+  def evaluate(environment)
+    Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
+  end
+end
